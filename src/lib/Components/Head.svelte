20fbd892dea3c2f3048928f1f5a14f0070948ee8
Ivan Semochkin <ivansemochkin@Ivans-MacBook-Pro.local>
2024-01-21 16:08:37 +0700

    update head

---
+++ src/lib/Components/Head.svelte
@@ -0,0 +1,47 @@
+<script lang="ts">
+	import { env } from '$env/dynamic/public';
+</script>
+
+<svelte:head>
+	<meta name="author" content="Ivan Semochkin" />
+	<meta
+		name="description"
+		content="Pathfinding Algorithms Visualizer build with Svelte and TypeScript"
+	/>
+
+	<meta
+		property="og:image"
+		content="https://ivan-sem.com/pathfinding_visualizer/static/algo-screen.png"
+	/>
+	<meta
+		property="og:image:secure_url"
+		content="https://ivan-sem.com/pathfinding_visualizer/static/algo-screen.png"
+	/>
+	<meta property="og:image:width" content="1200" />
+	<meta property="og:image:height" content="600" />
+	<meta
+		property="og:image:alt"
+		content="Pathfinding Algorithms Visualizer build with Svelte and TypeScript."
+	/>
+	<meta property="og:title" content="Pathfinding Visualizer" />
+	<meta property="og:type" content="object" />
+	<meta property="og:url" content="https://ivan-sem.com/pathfinding_visualizer" />
+	<meta
+		property="og:description"
+		content="Pathfinding Algorithms Visualizer build with Svelte and TypeScript."
+	/>
+	<meta property="og:locale" content="en_US" />
+	<meta name="theme-color" content="#22192B" />
+
+	<script async src="https://www.googletagmanager.com/gtag/js?id={env.PUBLIC_GTAG_KEY}"></script>
+	<script>
+		window.dataLayer = window.dataLayer || [];
+
+		function gtag() {
+			dataLayer.push(arguments);
+		}
+
+		gtag('js', new Date());
+		gtag('config', env.PUBLIC_GTAG_KEY);
+	</script>
+</svelte:head>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
+	import Head from '$lib/Components/Head.svelte';
 	import '../app.css';
 </script>
 
+<Head />
 <slot />
